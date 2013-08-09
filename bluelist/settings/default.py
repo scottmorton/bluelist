eb112d4b62c80893fc1626c9b54cce4541fbfb16
vagrant <vagrant@localhost.(none)>
2013-08-09 17:54:26 +0000

    newness

--- bluelist/settings/default.py
+++ bluelist/settings/default.py
@@ -79,7 +79,7 @@ USE_TZ = True
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# UasdRL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
