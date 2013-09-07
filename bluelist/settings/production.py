3cc60c8d153d6b204446d98d168c24b9ef51ecf1
vagrant <vagrant@localhost.(none)>
2013-09-07 06:19:52 +0000

    nice4

--- bluelist/settings/production.py
+++ bluelist/settings/production.py
@@ -3,4 +3,7 @@ from default import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-STATIC_URL = 'https://www.bluelist.us/static_prod/'
+STATIC_URL = '/static_prod/'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
