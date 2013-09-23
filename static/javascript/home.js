f728f4b23af95bc3f8557c886dfe72136cc4a0a6
vagrant <vagrant@localhost.(none)>
2013-09-23 04:15:12 +0000

    9_22_13_4

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -72,40 +72,57 @@ $(document).ready(function() {
 	$(document).on('click','#contact', function(event) {
 	   event.stopPropagation();
 	   
-	   if(signed_in=="true")
+	   if(signed_in!="false")
 	   {
-	   
-	   
 	   var selid = $(this).closest(".profile-index-expanded, .profile-index").attr("id");
        var pnum=Number(selid.slice(4));
        name= profiles[pnum].fields.name;
        public_email= profiles[pnum].fields.public_email;
        public_phone_num= profiles[pnum].fields.public_phone_num;
 
-       contact_html='<div class="lbox-container">                                                                          \
-                   <img class="lbox-close" id="lbox_close" src="/static/close.png" alt="close">                        \
-                   <div class="lbox-info">                                                                                  \
-                        <h4> Contact info provided by '+name+' </h4>\
-                        <table class="lbox-table" id="contact_info"> \
-                            <tr><td><p class="contact-label"><b>Email:</b></p></td><td> <p class="contact-text"> '+public_email+'</p></td></tr>\
-                            <tr><td><p class="contact-label"><b>Phone number:</b> </p></td><td> <p class="contact-text">'+public_phone_num+'</p></td></tr>\
-                        </table>\
-                   </div>                                                                                                   \
+       var contact_string="";
+       var contact_html="";
+
+       if(public_email=="" && public_phone_num=="")
+       {
+           contact_string='<p> We\'re sorry but '+name+' has not provided any public contact information';
+       }
+       else 
+       {
+           contact_string='<h4> Contact info provided by '+name+' </h4> \
+                                <table class="lbox-table" id="contact_info">';
+           
+           if(public_email!="")
+           {
+            contact_string=contact_string+'<tr><td><p class="contact-label"><b>Email:</b></p></td>\
+                                            <td> <p class="contact-text"> '+public_email+'</p></td></tr>';
+            }
+            
+            if(public_phone_num!="")
+            {
+            contact_string=contact_string+'<tr><td><p class="contact-label"><b>Phone number:</b> </p></td>\
+                                            <td> <p class="contact-text">'+public_phone_num+'</p></td></tr>';
+             }                           
+             
+             contact_string=contact_string+'</table>';
+
+         }
+
+       
+       contact_html='<div class="lbox-container">                                                           \
+                   <img class="lbox-close" id="lbox_close" src="/static/close.png" alt="close">                 \
+                   <div class="lbox-info">                                                                      \
+                    '+contact_string+'                                                                          \
+                   </div>                                                                                       \
              	   </div>';
        
-       
-       $(contact_html).lightbox_me({closeSelector:'#lbox_close',centered: true, 
-               overlayCSS:{background: 'black', opacity: .5},  onLoad: function() {
-           }, onClose: function(){$('.lbox-container').remove();} });
-           e.preventDefault();
-	    
-	   
+
 	   }
 	   
 	   else
 	   {
 	        
-	        contact_error_html='<div class="lbox-container">                                                                          \
+	        contact_html='<div class="lbox-container">                                                                          \
                            <img class="lbox-close" id="lbox_close" src="/static/close.png" alt="close">                        \
                            <div class="lbox-info">                                                                                  \
                                 <p> Please Login or Signup to view contact information</p> \
@@ -115,14 +132,13 @@ $(document).ready(function() {
                                 </div>\
                            </div>                                                                                                   \
                      	   </div>';
-	        
-	        
-           $(contact_error_html).lightbox_me({closeSelector:'#lbox_close',centered: true, 
+           }
+           
+           
+           $(contact_html).lightbox_me({closeSelector:'#lbox_close',centered: true, 
                    overlayCSS:{background: 'black', opacity: .5},  onLoad: function() {
                }, onClose: function(){$('.lbox-container').remove();} });
                e.preventDefault();
-           }
-	   
     });
 	
 	
